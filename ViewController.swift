1d0b4f3cdc326f4b540629305520335521931d08
GabeLops <gabrieldlops@gmail.com>
2019-12-04 22:30:58 -0800

    Final anagram game

--- ViewController.swift
+++ ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -28,10 +29,13 @@ class ViewController: UITableViewController {
         startGame()
     }
     
-    func startGame () {
+    @objc func startGame () {
+        let ac = UIAlertController(title: "New Game", message: "Try to make as many Anagrams as possible", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default))
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
+        present(ac, animated: true)
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return usedWords.count
@@ -57,38 +61,38 @@ class ViewController: UITableViewController {
     func submit (_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
-        let errorTitle: String
-        let errorMessage: String
-        
         if isPossible(word: lowerAnswer){
             if isOriginal(word: lowerAnswer){
                 if isReal(word: lowerAnswer){
-                    usedWords.insert(answer, at: 0)
+                    usedWords.insert(answer.lowercased(), at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
                     
                     return
                 }else{
-                    errorTitle = "Word not recognized"
-                    errorMessage = "Don't make it up"
+                    showErrorMessage(error: answer)
+                        
+                    
+                    
                 }
             }else {
-                errorTitle = "Word already used"
-                errorMessage = "Be more origninal"
+                showErrorMessage(error: answer)
+                
             }
         }else {
+            showErrorMessage(error: answer)
             
-            errorTitle = "Word not possible"
-            errorMessage = "You can't spell that word from \(title!.lowercased())."
+            
         }
-        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default))
-        present (ac, animated: true)
+
+        
+        
     }
     
     func isPossible(word: String) -> Bool {
         guard var tempWord = title?.lowercased() else{ return false }
+      
         
         for letter in word {
             if let position = tempWord.firstIndex(of: letter) {
@@ -103,16 +107,56 @@ class ViewController: UITableViewController {
     }
     
     func isOriginal (word: String) -> Bool {
-        return !usedWords.contains(word)
+        if word == title {return false}
+        return !usedWords.contains(word.lowercased())
     }
     
     func isReal(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        
         return misspelledRange.location == NSNotFound
     }
+    
+    func showErrorMessage(error: String){
+        var errorTitle: String
+        var errorMessage: String
+        
+        if !isReal(word: error){
+            
+        errorTitle = "Word not recognized or under 3 letters"
+        errorMessage = "Don't make it up, have more than 2 letter words"
+            
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default))
+        present (ac, animated: true)
+            
+        } else if !isOriginal(word: error){
+            
+        errorTitle = "Word already used"
+        errorMessage = "Be more origninal"
+            
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default))
+        present (ac, animated: true)
+            
+        }else if !isPossible(word: error){
+            
+        errorTitle = "Word not possible"
+        errorMessage = "You can't spell that word from \(title!.lowercased())."
+            
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default))
+        present (ac, animated: true)
+        }
+       
+
+       
+       
+        }
     
     
 
